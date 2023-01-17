711be6b9fe7c93001401bbe6f6c9e7eaf203a919
Sajeeth Ahmad <sajiiahd@gmail.com>
2023-01-17 23:04:04 +0530

    feat: create the logic

--- js-codes/rock-paper-scissor.js
+++ js-codes/rock-paper-scissor.js
@@ -1,3 +1,10 @@
 // Let's define the game play variable with 'confirm'
 
-let playGame = confirm("Shall we play the game?")
+let playGame = confirm("Shall we play the game?");
+
+if (playGame) {
+  let playerChoice = prompt("Play by typing rock or paper or scissor");
+
+}else {
+  alert("Alright, will see you next time!");
+}
